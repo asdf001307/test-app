adc0eccc1e9332f367904071a0ca1e45686064a7
asdf1307 <asdf001307@gmail.com>
2021-08-17 18:11:07 +0900

    0817

---
+++ backend/routes/users.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+const bcrypt = require('bcryptjs');
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.post('/signUp', function (req, res) {
+  const user = {
+    'name': req.body.user.name,
+    'password': req.body.user.password
+  };
+
+      const salt = bcrypt.genSaltSync();
+      const encryptedPassword = bcrypt.hashSync(user.pw, salt);
+      connection.query('INSERT INTO auto_test VALUES ("' + user.name + '","' + encryptedPassword + '")', user, function (err, row2) {
+        if (err) throw err;
+      });
+      res.json({
+        success: true,
+        message: 'Sing Up Success!'
+      })
+})
+module.exports = router;
